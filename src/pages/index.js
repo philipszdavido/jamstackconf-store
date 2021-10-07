8b244ba5fabe279ad7efb11ffc4f4bec929824aa
philipszdavido <kurtwanger40@gmail.com>
2021-10-07 21:21:42 +0100

    Added Fuse.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { FaShoppingCart } from "react-icons/fa";
+import Fuse from "fuse.js";
 
 import Layout from "@components/Layout";
 import Container from "@components/Container";
@@ -12,9 +14,27 @@ import products from "@data/products.json";
 import styles from "@styles/Home.module.scss";
 
 export default function Home() {
-  // function handleOnSearch() {
-  //   // Do something here
-  // }
+  const [query, setQuery] = useState();
+  let activeProducts = products;
+  const fuse = new Fuse(products, {
+    keys: ["title"],
+  });
+
+  if (query) {
+    activeProducts = fuse.search(query).map((result) => {
+      return { ...result.item };
+    });
+  }
+
+  function handleOnSearch(event) {
+    // Do something here
+    /*const filteredProducts = products.filter((product) =>
+      product.title.toLowerCase().includes(query && query.toLowerCase())
+    );
+    console.log(event.currentTarget.value);*/
+    setQuery(event.currentTarget.value);
+  }
+
   return (
     <Layout>
       <Head>
@@ -23,35 +43,29 @@ export default function Home() {
           name="description"
           content="Your favorite trading cards delivered!"
         />
-        <link rel="preconnect" href="https://app.snipcart.com" />
-        <link rel="preconnect" href="https://cdn.snipcart.com"></link>
-        <link
-          rel="stylesheet"
-          href="https://cdn.snipcart.com/themes/v3.2.1/default/snipcart.css"
-        />
       </Head>
 
       <Container>
         <h1 className="sr-only">Hyper Bros. Trading Cards</h1>
 
-        {/* <div className={styles.discover}>
+        <div className={styles.discover}>
           <div className={styles.search}>
             <h2>Search</h2>
             <form>
               <input onChange={handleOnSearch} type="search" />
             </form>
           </div>
-        </div> */}
+        </div>
 
-        <p className={styles.cart}>
+        {/*<p className={styles.cart}>
           <FaShoppingCart />
           <span className="snipcart-total-price">$0.00</span>
           <Button className="snipcart-checkout">View Cart</Button>
-        </p>
+        </p> */}
 
         <h2 className="sr-only">Available Cards</h2>
         <ul className={styles.products}>
-          {products.map((product) => {
+          {activeProducts.map((product) => {
             return (
               <li key={product.id}>
                 <Link href={`/products/${product.id}`}>
@@ -86,15 +100,6 @@ export default function Home() {
           })}
         </ul>
       </Container>
-      <script
-        async
-        src="https://cdn.snipcart.com/themes/v3.2.1/default/snipcart.js"
-      ></script>
-      <div
-        hidden
-        id="snipcart"
-        data-api-key={process.env.NEXT_PUBLIC_SNIPCART_KEY}
-      ></div>
     </Layout>
   );
 }
--- src/pages/products/[productId].js
+++ src/pages/products/[productId].js
@@ -1,64 +1,80 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import Head from "next/head";
+import Image from "next/image";
 
-import Layout from '@components/Layout';
-import Header from '@components/Header';
-import Container from '@components/Container';
-import Button from '@components/Button';
+import Layout from "@components/Layout";
+import Header from "@components/Header";
+import Container from "@components/Container";
+import Button from "@components/Button";
 
-import products from '@data/products.json';
+import products from "@data/products.json";
 
-import styles from '@styles/Product.module.scss'
+import styles from "@styles/Product.module.scss";
 
 export default function Product({ product }) {
   return (
     <Layout>
       <Head>
-        <title>{ product.title }</title>
-        <meta name="description" content={`Find ${product.title} at Hyper Bros Trading`} />
+        <title>{product.title}</title>
+        <meta
+          name="description"
+          content={`Find ${product.title} at Hyper Bros Trading`}
+        />
       </Head>
 
       <Container>
         <div className={styles.productWrapper}>
           <div className={styles.productImage}>
-            <Image width="864" height="1200" src={product.image} alt={product.title} />
+            <Image
+              width="864"
+              height="1200"
+              src={product.image}
+              alt={product.title}
+            />
           </div>
           <div className={styles.productContent}>
-            <h1>{ product.title }</h1>
-            <p className={styles.price}>
-              ${ product.price }
-            </p>
+            <h1>{product.title}</h1>
+            <p className={styles.price}>${product.price}</p>
             <p>
-              <Button>Add to Cart</Button>
+              <Button
+                className="snipcart-add-item"
+                data-item-id={product?.id}
+                data-item-price={product?.price}
+                data-item-url={`/products/${product.id}`}
+                data-item-description={product?.title}
+                data-item-image={product?.image}
+                data-item-name={product?.title}
+              >
+                Add to Cart
+              </Button>
             </p>
           </div>
         </div>
       </Container>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps({ params }) {
-  const product = products.find(product => product.id === params.productId);
+  const product = products.find((product) => product.id === params.productId);
 
   return {
     props: {
-      product
-    }
-  }
+      product,
+    },
+  };
 }
 
 export async function getStaticPaths() {
-  const paths = products.map(product => {
+  const paths = products.map((product) => {
     return {
       params: {
-        productId: product.id
-      }
+        productId: product.id,
+      },
     };
   });
 
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
